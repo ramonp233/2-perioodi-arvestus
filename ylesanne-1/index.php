694fde2447196d4940194441a64a835bb33ad9d6
ramonp233 <ramonp233@gmail.com>
2014-11-25 13:59:50 +0200

    Ülesanne 1 Finish

--- ylesanne-1/index.php
+++ ylesanne-1/index.php
@@ -4,7 +4,15 @@
     <meta charset="UTF-8">
     <title>II perioodi arvestustöö - Ülesanne 1 - GET päring</title>
   </head>
-
   <body>
+  <?php
+
+        $result_link = "result.php";
+        $username = "Ramon";
+        $age = 17;
+  ?>
+
+  <a href="<?php echo $result_link.'?username='.$username.'&age='.$age; ?>">Result</a>
+
   </body>
 </html>
--- ylesanne-1/result.php
+++ ylesanne-1/result.php
@@ -1,11 +1,23 @@
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
     <title>II perioodi arvestustöö - Ülesanne 1 - GET päringu tagasiside</title>
   </head>
-
   <body>
     <a href="index.php">Mine tagasi</a>
+    <?php
+    if(isset($_GET['age'])) {
+        if ($_GET['age'] >= 18 ) {
+            echo "Tere, {$_GET['username']}! Olete lehekülje vaatamiseks piisavalt vana. ";
+        }
+        else {
+            echo "Tere, {$_GET['username']}! Kahjuks olete liiga noor selle lehekülje külastamiseks.";
+        }
+    }else{
+        echo "Tere! Teie kasutajanimi ja vanus ei ole defineeritud. ";
+    }
+    ?>
   </body>
 </html>
